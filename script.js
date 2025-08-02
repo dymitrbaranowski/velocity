e984a27fce4c5764d545ae09d6a4d8f1c8e8053c
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-08-02 20:15:58 +0300

    fox3

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ tabsBtns.forEach((btn, index) =>
 );
 
 //Anchors-----------
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach((anc) => {
+  anc.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    const elem = document.querySelector(id);
+    window.scroll({
+      top: elem.offsetTop - 80,
+      behavior: "smooth",
+    });
+  });
+});
